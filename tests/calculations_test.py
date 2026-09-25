49837b90c82dabb89fa472fee3933816bd1d7808
Lucas Borges <lucaspdfborges@gmail.com>
2026-09-25 20:51:49 +0000

    feat: improve coverage

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -10,6 +10,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../s
 from calculations import area_of_circle, get_nth_fibonacci   # noqa: E402
 
 
+def test_area_of_circle_negative_radius():
+    """Test with a negative radius."""
+    with pytest.raises(ValueError):
+        area_of_circle(-1)
+
+
 def test_area_of_circle_positive_radius():
     """Test with a positive radius."""
     # Arrange
